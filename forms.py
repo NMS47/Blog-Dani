ffc94132e8d1684aca46000c78a3ea635da605ca
NMS47 <nicolas.salvadores93@gmail.com>
2022-09-24 16:13:40 -0300

    Listo para subir

--- forms.py
+++ forms.py
@@ -8,6 +8,6 @@ class CreatePostForm(FlaskForm):
     title = StringField("Título", validators=[DataRequired()])
     subtitle = StringField("Subtitulo", validators=[DataRequired()])
     category = SelectField(u"Categoría", choices=['Viajes', 'Escalada', 'Montañismo', 'Otra'])
-    img_url = StringField("URL de la imagen", validators=[DataRequired(), URL()])
+    img_url = StringField("URL de la imagen", default="http://drive.google.com/uc?export=view&id=", validators=[DataRequired(), URL()])
     body = CKEditorField("Contenido", validators=[DataRequired()])
     submit = SubmitField("Subir Post")
