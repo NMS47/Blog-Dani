29951c6663fad3bacc5b067256f3b90b3512de76
NMS47 <nicolas.salvadores93@gmail.com>
2022-09-28 03:04:31 -0300

    trying without os.environ

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ from functools import wraps
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-import smtplib, ssl
+import smtplib
 import os
 
 Base = declarative_base()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 ##GRAVATAR
@@ -294,4 +294,4 @@ def delete_post(post_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
