6d50b35923a0484bbc4e73d728783fe54e4ab4e3
NMS47 <nicolas.salvadores93@gmail.com>
2024-02-09 09:58:39 -0300

    updated db variables

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL1', "sqlite:///dani-blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL2', "sqlite:///dani-blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
